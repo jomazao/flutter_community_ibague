a9218ce9dce41ec7c68aab9734c2dbe0eb88a2fc
Juan <juan.davila@sofka.com.co>
2024-02-07 10:13:43 -0500

    Added new favicon and debug show mode banner set to false

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Flutter Community Ibagué',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
