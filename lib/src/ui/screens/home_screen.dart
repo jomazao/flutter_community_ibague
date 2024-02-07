224dd4c654751f07435bd3999721f0e3baa41e5e
Juan <juan.davila@sofka.com.co>
2024-02-07 09:48:03 -0500

    Added drawer and appbar

--- lib/src/ui/screens/home_screen.dart
+++ lib/src/ui/screens/home_screen.dart
@@ -11,6 +11,46 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text('Flutter Community Ibague'),
+        leading: IconButton(
+          icon: const Icon(Icons.search),
+          onPressed: () {},
+        ),
+        elevation: 1.0,
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.all(8.0),
